065d989f8512851eb872fd1737f4c1dd4588a898
David Malášek <davidmalasek@post.cz>
2021-04-27 20:02:16 +0200

    Medium changes - modified the order in index.js - minor changes + additions in override.css - fixed themes + added component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./css/normalize.css";
+import "./css/bootstrap.css";
 import "./css/skywalker.css";
 import "./css/skywalker-themes.css";
-import "./css/bootstrap.css";
 import "./css/override.css";
